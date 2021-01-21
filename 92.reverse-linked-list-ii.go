b60a4f4d0f1edf29908e8b963d71d7d128bd5c32
zhangruobin001 <zhangruobin001@ke.com>
2021-01-21 11:11:41 +0800

    [nb] Commit

--- 92.reverse-linked-list-ii.go
+++ 92.reverse-linked-list-ii.go
@@ -12,16 +12,16 @@
  * Testcase Example:  '[1,2,3,4,5]\n2\n4'
  *
  * Reverse a linked list from position m to n. Do it in one-pass.
- * 
+ *
  * Note: 1 ≤ m ≤ n ≤ length of list.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: 1->2->3->4->5->NULL, m = 2, n = 4
  * Output: 1->4->3->2->5->NULL
- * 
- * 
+ *
+ *
  */
 /**
  * Definition for singly-linked list.
@@ -30,6 +30,35 @@
  *     Next *ListNode
  * }
  */
+
+var p *ListNode
+
 func reverseBetween(head *ListNode, m int, n int) *ListNode {
-    
+	if m == 1 {
+		return reverseN(head, n)
+	}
+	head.Next = reverseBetween(head.Next, m-1, n-1)
+	return head
+}
+
+func reverse(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	last := reverse(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
+
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		p = head.Next
+		return head
+	}
+
+	last := reverseN(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = p
+	return last
 }
